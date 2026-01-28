097253125312f75a606504e1def757c761a513fd
sarfall-sys <sarfall00@gmail.com>
2026-01-28 09:03:42 -0500

    first commit

--- zom-management-frontend/src/components/CanManageProducts.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CanManageProducts() {
-  return (
-    <div>CanManageProducts</div>
-  )
-}
-
-export default CanManageProducts
--- zom-management-frontend/src/pages/admin/UserCreate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function UserCreate() {
-  return (
-    <div>UserCreate</div>
-  )
-}
-
-export default UserCreate
